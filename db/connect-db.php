5ba3ddb8b9fc31ed2c8f371362153ddb71771533
kkm005 <33860679+kkm005@users.noreply.github.com>
2018-10-11 11:39:42 +0700

    Add files via upload

--- db/connect-db.php
+++ db/connect-db.php
@@ -1,11 +1,12 @@
 <?php
 	$server = "us-cdbr-iron-east-01.cleardb.net";
-	$username = "b58baa02f9ab8a";
-	$password = "797139ad";
-	$db = "heroku_ea07ab03ff13442";
+	$username = "bb638a0b9e5724";
+	$password = "3556cc19";
+	$db = "heroku_5663ecc9ac15f3e";
     $conn = new mysqli($server, $username, $password, $db);
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
     mysqli_query($conn, "SET NAMES utf8");
+?>
